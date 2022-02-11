913188097885c2572a6e3a041eefd03c12f2bc42
Yoshiaki Hara <>
2022-02-11 12:28:46 +0900

    add vim plugin install process

--- setup.sh
+++ setup.sh
@@ -11,6 +11,10 @@ mkln zshrc
 mkln tmux.conf
 mkln p10k.zsh
 
+# install vundle for vim 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle
+vim +PluginInstall +qall
+
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 ln -s ${PWD}/colors ~/.config/nvim/colors
 ln -s ${PWD}/colors ~/.vim/colors
